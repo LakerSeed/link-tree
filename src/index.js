f264ca5385f33442b1462da34be31851aa63aeb5
André Rangel <andrerangel277@gmail.com>
2025-08-26 22:24:25 -0300

    feat: adiciona lógica de troca de tema

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const theme_switch = document.getElementById("theme")
+const html = document.body
+theme_switch.addEventListener("change",function myfunction(){
+    html.classList.toggle("light")
+    // alert("pare com isso")
+}) 
+// theme_switch.addEventListener("change",)
