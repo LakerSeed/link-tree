5fc746b3106f549f6115fc5da61e99b04b9580ab
André Rangel <andrerangel277@gmail.com>
2025-08-26 23:04:28 -0300

    feat: adiciona lógica de links no botôes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const theme_switch = document.getElementById("theme")
 const html = document.body
 theme_switch.addEventListener("change",function myfunction(){
     html.classList.toggle("light")
-    // alert("pare com isso")
 }) 
-// theme_switch.addEventListener("change",)
+function gotolink(link){
+    alert(link.value)
+}
