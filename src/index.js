237af30cd0dd0cb747c909757fe6b1ea9fdd4c7b
André Rangel <andrerangel277@gmail.com>
2025-08-30 15:02:58 -0300

    feat: adiciona lógica de confirmação do link

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 const theme_switch = document.getElementById("theme")
 const html = document.body
+const buttons = document.querySelectorAll(".button")
+const modal = document.getElementById("modal")
+
+//theme switch logic
 theme_switch.addEventListener("change",function myfunction(){
     html.classList.toggle("light")
-}) 
+})
+
+// variable that stores a link value
+let address
+
+// this function show the modal and assing a value to adress
 function gotolink(link){
-    alert(link.value)
+    //reset the animation
+    modal.classList.remove('disappear')
+    modal.classList.add('appear')
+    modal.style.display = "flex"
+    address = link.value
+}
+//this function manage if the user really want go to another page
+function confirm(i){
+    if(i.value == "open"){
+        window.open(address)
+        modal.classList.remove('appear')
+        modal.classList.add('disappear')
+        setTimeout(closeModal, 500)
+    }
+    else
+        modal.classList.remove('appear')
+        modal.classList.add('disappear')
+        setTimeout(closeModal, 500)
+}
+function closeModal(){
+    modal.style.display = "none"
 }
